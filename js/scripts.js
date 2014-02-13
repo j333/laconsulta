7aa7e9efdfe173b21e5c7f96d89bfca00f4dd961
Adriel Z�rate <adriel_zarate@hotmail.com>
2014-02-13 01:14:06 -0300

    maquetado en proceso 2
    seccion de vinos, font-awesome, auto scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
-$(document).ready(function(){
+			$(document).ready(function(){
+
+				var anchoVentana = $(window).width();
+				var altoVentana = $(window).height();
+
+				$("#vinos img").css("height", altoVentana + "px");
+
                 var $windows = $('section');
                 $windows.windows({
                     snapping: true,
@@ -25,6 +31,25 @@ $(document).ready(function(){
                 }
             });
 
+            $('#reserva > a').click(function () {
+                $("#modal_reserva").fadeIn("slow");
+                return false;
+            });
+            $('#modal_reserva > a').click(function () {
+                $("#modal_reserva").fadeOut("slow");
+				$('html, body').animate({ scrollTop: $("#vinos").offset().top }, 0);
+                return false;
+            });
+
+            $('#varietal > a').click(function () {
+                $("#modal_varietal").fadeIn("slow");
+                return false;
+            });
+            $('#modal_varietal > a').click(function () {
+                $("#modal_varietal").fadeOut("slow");
+                $('html, body').animate({ scrollTop: $("#vinos").offset().top }, 0);
+                return false;
+            });
 
 			var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
 				showLeftPush = document.getElementById( 'showLeftPush' ),
