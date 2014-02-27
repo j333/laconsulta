f95103de38656e094ce87a3330301cbfa303a0c8
Adriel Z�rate <adriel_zarate@hotmail.com>
2014-02-27 00:44:10 -0300

    cambios varios de estilo

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
 				var anchoVentana = $(window).width();
 				var altoVentana = $(window).height();
 
-				$("#vinos img").css("height", altoVentana + "px");
+				$("#vinos .col > img").css("height", altoVentana -100);
 
                 var $windows = $('section');
                 $windows.windows({
@@ -15,16 +15,79 @@
                     onWindowEnter: function($el){}
                 });
 
-				//funcion que detecta si el video termino, y vuelve a verse la imagen de frente con fadeIn
-				$("#video_inicio").bind("ended", function() {
-				    $('#video_background').fadeIn("slow");
-				});
 			});
 
+//funcion que detecta si el video termino, y vuelve a verse la imagen de frente con fadeIn
+                $("#video_inicio").bind("ended", function() {
+                    $('#video_background').fadeIn("slow");
+                    $(".detener_video").fadeOut();
+                });
+
+                $(".detener_video").click(function(){
+                    $(this).fadeOut();
+                    $('#video_inicio')[0].stop();
+                });
+
+/*ir a la seccion especifica seleccionando desde el menu*/
+                $("#btn_inicio").click(function() {
+                    $("nav a").removeClass("seleccionado");
+                    $('html, body').animate({
+                        scrollTop: $("#inicio").offset().top
+                    }, 1000);
+                });
+
+                $("#btn_historia").click(function() {
+                    $("nav a").removeClass("seleccionado");
+                    $('html, body').animate({
+                        scrollTop: $("#historia").offset().top
+                    }, 1000,function(){
+                        $("#btn_historia").addClass("seleccionado");
+                    });
+                });
+
+                $("#btn_vinos").click(function() {
+                    $("nav a").removeClass("seleccionado");
+                    $('html, body').animate({
+                        scrollTop: $("#vinos").offset().top
+                    }, 1000,function(){
+                        $("#btn_vinos").addClass("seleccionado");
+                    });
+                });
+
+                $("#btn_tressesenta").click(function() {
+                    $("nav a").removeClass("seleccionado");
+                    $('html, body').animate({
+                        scrollTop: $("#tressesenta").offset().top
+                    }, 1000,function(){
+                        $("#btn_tressesenta").addClass("seleccionado");
+                    });
+                });
+
+                $("#btn_enoturismo").click(function() {
+                    $("nav a").removeClass("seleccionado");
+                    $('html, body').animate({
+                        scrollTop: $("#enoturismo").offset().top
+                    }, 1000,function(){
+                        $("#btn_enoturismo").addClass("seleccionado");
+                    });
+                });
+
+                $("#btn_contacto").click(function() {
+                    $("nav a").removeClass("seleccionado");
+                    $('html, body').animate({
+                        scrollTop: $("#contacto").offset().top
+                    }, 1000,function(){
+                        $("#btn_contacto").addClass("seleccionado");
+                    });
+                });
+
+/***********************/
+
 			$('#video_background').click(function () {
-				$(this).fadeOut("slow");
+				$(this).fadeOut("slow");             
                 if ($("#video_inicio").get(0).paused) {
                     $("#video_inicio").get(0).play();
+                    $(".detener_video").fadeIn("slow");   
                 }
                 else {
                     $("#video_inicio").get(0).pause();
